9005f1539cf3a00e98a45a23e343e3f95823929a
Gabriel Melo <gmelomartins@apption.com>
2024-03-11 02:27:51 -0400

    salary with inflation adjusted cleanups

--- src/clean_salary.py
+++ src/clean_salary.py
@@ -10,8 +10,13 @@ df_salary_inflation = pd.DataFrame(columns=cols)
 for year in ['2020','2021','2022','2023']:
     file = os.path.join(os.getcwd(), 'data','raw','salaries',f'{year}.csv')  
     df = pd.read_csv(file)
+    
+    # Salary data
     df_salary['player'] = df['Player']
     df_salary[year] = df['Salary']
+
+    # Salary data adjusted for inflation
+    df_salary_inflation['player'] = df['Player']
     df_salary_inflation[year] = df['Salary (adjusted)']
 
 # Save the data
