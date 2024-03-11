29c307cc29afa4b9f693135c6ce2cc00f7854d19
Gabriel Melo <gmelomartins@apption.com>
2024-03-11 19:56:06 -0400

    added joined tables

--- src/clean_salary.py
+++ src/clean_salary.py
@@ -4,20 +4,31 @@ import pandas as pd
 cols = ['player', '2020', '2021', '2022', '2023']
 
 df_salary = pd.DataFrame(columns=cols)
-df_salary_inflation = pd.DataFrame(columns=cols)
+df_salary_inflation = pd.DataFrame(columns=[c + ' (adjusted)' if c != 'player' else 'player' for c in cols ])
 
 # Load the data
 for year in ['2020','2021','2022','2023']:
     file = os.path.join(os.getcwd(), 'data','raw','salaries',f'{year}.csv')  
     df = pd.read_csv(file)
-    
-    # Salary data
-    df_salary['player'] = df['Player']
-    df_salary[year] = df['Salary']
 
-    # Salary data adjusted for inflation
-    df_salary_inflation['player'] = df['Player']
-    df_salary_inflation[year] = df['Salary (adjusted)']
+    # Rename columns
+    df.rename(columns={'Player': 'player'}, inplace=True)
+    df.rename(columns={'Salary':year}, inplace=True)
+    df.rename(columns={'Salary (adjusted)':year + ' (adjusted)'}, inplace=True)
+
+
+    # Insert salary data
+    if df_salary.empty:
+        df_salary = df[['player', year]]
+        df_salary_inflation = df[['player', year + ' (adjusted)']]
+
+    else:
+        df_salary = df_salary.merge(df[['player', year]], on='player', how='outer')
+        df_salary_inflation = df_salary_inflation.merge(df[['player', year + ' (adjusted)']], on='player', how='outer')
+
+    # Change floats to ints, ignore NaN
+    df_salary[year] = df_salary[year].astype('Int64',errors='ignore')
+    df_salary_inflation[year + ' (adjusted)'] = df_salary_inflation[year + ' (adjusted)'].astype('Int64',errors='ignore')
 
 # Save the data
 df_salary.to_csv(os.path.join(os.getcwd(), 'data','clean','salaries','salary.csv'), index=False)
--- src/table_join.py
+++ src/table_join.py
@@ -1,20 +1,72 @@
 import pandas as pd
 import os
 
-# Join tables per year and player name
-def join_tables():
-    # Read all files in the directory
-    files = os.listdir('data/clean/salaries')
-    files = [file for file in files if file.endswith('.csv')]
+years = ['20','21','22','23']
 
-    # Create a list of dataframes
-    dfs = [pd.read_csv(f'data/clean/salaries/{file}') for file in files]
+# Join every year's tables
+for year in years:
+    data_folder = os.path.join(os.getcwd(),'data', 'clean')
+    ratings = pd.read_csv(os.path.join(data_folder, '2k ratings','2k'+year+'_clean.csv'))
+    salary = pd.read_csv(os.path.join(data_folder, 'salaries','salary.csv'))
+    stats = pd.read_csv(os.path.join(data_folder, 'stats',f'{str(int(year)-1)}-{year} Regular.csv'))
 
-    # Join dataframes
-    df = pd.concat(dfs, axis=0)
+    # Join ratings and stats by player name
+    ratings_stats = ratings.merge(stats, left_on='name', right_on='Player', how='inner')
+    ratings_stats = ratings_stats.drop(columns=['Player'])
+    # ratings_stats.to_csv('2k'+year+'_rating_stats.csv', index=False)
 
-    # Export the dataframe to a csv file
-    df.to_csv('data/raw/salaries/all.csv', index=False)
+    # Save players that didn't match between ratings and stats
+    ratings_names = ratings['name'].values
+    stats_names = stats['Player'].values
+    missing_names = list(set(ratings_names) - set(stats_names))
+    missing = ratings[ratings['name'].isin(missing_names)]
+    missing.to_csv('20'+year+'_missing_stats.csv', index=False)
 
-    return df
+    # Add salary to the joined table
+    ratings_salary_stats = ratings_stats.merge(salary, left_on='name', right_on='player', how='inner')
+    ratings_salary_stats = ratings_salary_stats.drop(columns=['player'])
 
+    # Save players that didn't match between joined stats and salary
+    ratings_stats_names = ratings_stats['name'].values
+    salary_names = salary['player'].values
+    missing_salary_names = list(set(ratings_stats_names) - set(salary_names))
+
+    missing_salary = ratings_stats[ratings_stats['name'].isin(missing_salary_names)]
+    missing_salary.to_csv('20'+year+'_missing_salaries.csv', index=False)
+
+    # Save the joined table
+    ratings_salary_stats.to_csv('20'+year+'_joined.csv', index=False)
+
+# # Join 2020 tables per year and player name
+# data_folder = os.path.join(os.getcwd(),'data', 'clean')
+
+# ratings = pd.read_csv(os.path.join(data_folder, '2k ratings','2k20_clean.csv'))
+# salary = pd.read_csv(os.path.join(data_folder, 'salaries','salary.csv'))
+# stats = pd.read_csv(os.path.join(data_folder, 'stats','19-20 Regular.csv'))
+
+# # Join ratings and stats by player name
+# ratings_stats = ratings.merge(stats, left_on='name', right_on='Player', how='inner')
+# ratings_stats = ratings_stats.drop(columns=['Player'])
+# ratings_stats.to_csv('2k20_rating_stats.csv', index=False)
+
+# # Save players that didn't match between ratings and stats
+# ratings_names = ratings['name'].values
+# stats_names = stats['Player'].values
+# missing_names = list(set(ratings_names) - set(stats_names))
+# missing = ratings[ratings['name'].isin(missing_names)]
+# missing.to_csv('2020_missing_stats.csv', index=False)
+
+# # Add salary to the joined table
+# ratings_salary_stats = ratings_stats.merge(salary, left_on='name', right_on='player', how='inner')
+# ratings_salary_stats = ratings_salary_stats.drop(columns=['player'])
+
+# # Save players that didn't match between joined stats and salary
+# ratings_stats_names = ratings_stats['name'].values
+# salary_names = salary['player'].values
+# missing_salary_names = list(set(ratings_stats_names) - set(salary_names))
+
+# missing_salary = ratings_stats[ratings_stats['name'].isin(missing_salary_names)]
+# missing_salary.to_csv('2020_missing_salaries.csv', index=False)
+
+# # Save the joined table
+# ratings_salary_stats.to_csv('2020_joined.csv', index=False)
