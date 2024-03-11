6d80b93e6baf7e46ea2e3be43a3ddca8a138cb25
Gabriel Melo <gmelomartins@apption.com>
2024-03-11 02:03:57 -0400

    2k ratings cleaned

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-Salaries - https://hoopshype.com/salaries/players/2019-2020/
-Salaries (other) - https://www.espn.com/nba/salaries
-2024 Salaries - https://www.basketball-reference.com/contracts/players.html
+## Data sources
+### Salary 
+- Salaries - https://hoopshype.com/salaries/players/2019-2020/
+- Salaries (other) - https://www.espn.com/nba/salaries
+- 2024 Salaries - https://www.basketball-reference.com/contracts/players.html
 
-Stats - https://www.basketball-reference.com/leagues/NBA_2021_totals.html
-Stats (other) - official NBA website
+### Game stats 
+- Stats - https://www.basketball-reference.com/leagues/NBA_2021_totals.html
+- Stats (alternative) - official NBA website
 
-2k ratings OVERALL - https://hoopshype.com/nba2k/2018-2019/
-2k ratings json - https://github.com/MikeYan01/nba2k-player-ratings/
+### 2k ratings
+- 2kdb ratings api - https://2kdb.net/api/players/24/{"freeAgents":false,"page":"1","pageSize":"100","version":"24"}
+    - Tip: use headers={'User-Agent': 'Mozilla/5.0'}
+- 2k overall ratings - https://hoopshype.com/nba2k/2018-2019/
--- src/ratings_clean.py
+++ src/ratings_clean.py
@@ -5,10 +5,10 @@ import pandas as pd
 import os
 import unidecode as ud
 
-files = ['22 roster.json', '23 roster.json', '24 roster.json']
+json_files = ['22 roster.json', '23 roster.json', '24 roster.json']
 
 # Read JSON files
-for file in files:
+for file in json_files:
     with open(f'{os.getcwd()}/data/raw/2k detailed/{file}', encoding='utf-8') as f:
         data = json.load(f)
 
@@ -25,6 +25,24 @@ for file in files:
     # Order by player name
     df = df.sort_values(by='name')
 
-
     # Export to CSV
     df.to_csv(f'{os.getcwd()}/data/clean/2k ratings/2k{file[:2]}.csv', index=False)
+
+# Read CSV files
+csv_files = ['20 roster.csv']
+
+for file in csv_files:
+    df = pd.read_csv(f'{os.getcwd()}/data/raw/2k detailed/{file}')
+
+    # Remove columns Nationality,Team,Archetype,Position1,Position2,Height,Weight,Jersey
+    df = df.drop(columns=['Nationality','Team','Archetype','Position1','Position2','Height','Weight','Jersey','Year(s) in the NBA','Prior to NBA','Rank current','Rank all','Team_Category'])
+
+    # Clean player names
+    df['player'] = df['player'].apply(lambda x: x.replace('’', "'"))
+    df['player'] = df['player'].apply(lambda x: ud.unidecode(x))
+
+    # Order by player name
+    df = df.sort_values(by='player')
+
+    # Export to CSV
+    df.to_csv(f'{os.getcwd()}/data/clean/2k detailed/2k{file[:2]}.csv', index=False)
